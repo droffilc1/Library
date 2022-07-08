320c5df0b68e4d27877b96a77b25947f564ac2a8
Clifford Mapesa <cliffordmapesa8@gmail.com>
2022-07-08 18:55:22 +0300

    updated javascript

--- js/home.js
+++ js/home.js
@@ -1,71 +1,71 @@
 firebase.auth().onAuthStateChanged((user)=> {
-  if(user) {
+    if(user) {
 
       // pull book status
 
-      firebase.firestore().collection("bookStatus").get().then((querySnapshot)=> {
-          let content = '';
+        firebase.firestore().collection("bookStatus").get().then((querySnapshot)=> {
+            let content = '';
 
-          querySnapshot.forEach((doc) =>{
-              let bStatus = doc.data().bStatusInput;
-              let docId = doc.data().docId;   
-              
-              content += '<option value="'+bStatus+'">'+bStatus+'</option>';
-          })
+            querySnapshot.forEach((doc) =>{
+                let bStatus = doc.data().bStatusInput;
+                let docId = doc.data().docId;   
+                
+                content += '<option value="'+bStatus+'">'+bStatus+'</option>';
+            })
 
-          $("#bookStatus").append(content)
-      })
+            $("#bookStatus").append(content)
+        })
 
       // add book
-      document.getElementById("addBook").onclick = () => {
-          let bookTitle = document.getElementById("bookTitle").value; 
-          let bookDate = document.getElementById("bookDate").value;         
-          let bookAuthor = document.getElementById("bookAuthor").value;
-          let bookPages = document.getElementById("bookPages").value;
-          let bookStatus = document.getElementById("bookStatus").value;
+        document.getElementById("addBook").onclick = () => {
+            let bookTitle = document.getElementById("bookTitle").value; 
+            let bookDate = document.getElementById("bookDate").value;         
+            let bookAuthor = document.getElementById("bookAuthor").value;
+            let bookPages = document.getElementById("bookPages").value;
+            let bookStatus = document.getElementById("bookStatus").value;
 
-          let addBook = firebase.firestore().collection("books").doc();
-          addBook.set({
-              docId:addBook.id,
-              bookDate:bookDate,           
-              bookTitle:bookTitle,
-              bookAuthor:bookAuthor,
-              bookPages:bookPages,
-              bookStatus:bookStatus
-          }).then(() => {
-              window.location.reload();
-          })
-          
-      }
+            let addBook = firebase.firestore().collection("books").doc();
+            addBook.set({
+                docId:addBook.id,
+                bookDate:bookDate,           
+                bookTitle:bookTitle,
+                bookAuthor:bookAuthor,
+                bookPages:bookPages,
+                bookStatus:bookStatus
+            }).then(() => {
+                window.location.reload();
+            })
+            
+        } 
 
       // view books
 
-      firebase.firestore().collection("books").get().then((querySnapshot)=>{
-          let content = '';
-          querySnapshot.forEach((doc)=>{
+        firebase.firestore().collection("books").get().then((querySnapshot)=>{
+            let content = '';
+            querySnapshot.forEach((doc)=>{
 
-              
-              let bookTitle = doc.data().bookTitle;
-              let bookAuthor = doc.data().bookAuthor;
-              let bookPages = doc.data().bookPages;
-              let bookStatus = doc.data().bookStatus;
-              let docId = doc.data().docId;
+                
+                let bookTitle = doc.data().bookTitle;
+                let bookAuthor = doc.data().bookAuthor;
+                let bookPages = doc.data().bookPages;
+                let bookStatus = doc.data().bookStatus;
+                let docId = doc.data().docId;
 
-              content+=    '<tr>';
-              content+=    '  <td> ' + bookTitle +'</td>';              
-              content+=    '  <td> ' + bookAuthor +'</td>';
-              content+=    '  <td> ' + bookPages +'</td>';
-              content+=    '  <td> ' + bookStatus +'</td>';
-              content+=    '  <td> <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button> </td>'; 
-              content+=    '  <td> <button class="btn btn-danger">Delete</button> </td>';                    
-              content+=    '</tr>';
+                content+=    '<tr>';
+                content+=    '  <td> ' + bookTitle +'</td>';              
+                content+=    '  <td> ' + bookAuthor +'</td>';
+                content+=    '  <td> ' + bookPages +'</td>';
+                content+=    '  <td> ' + bookStatus +'</td>';
+                content+=    '  <td> <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button> </td>'; 
+                content+=    '  <td> <button class="btn btn-danger">Delete</button> </td>';                    
+                content+=    '</tr>';
 
-          })
-          $("#bookList").append(content);
-      })
+            })
+            $("#bookList").append(content);
+        })
 
 
-      window.editBook = (value) => {
+        window.editBook = (value) => {
         // alert(value)
         // invoke firebase
         db.collection("books").doc(value)
@@ -95,9 +95,7 @@ firebase.auth().onAuthStateChanged((user)=> {
                 console.log(error.message);
             })
         }
-
-    }
-    // delete tweet
+        // delete tweet
         window.deleteTweet = (docId) => {
             db.collection("books").doc(docId).delete().then(() => {              
                 
@@ -108,8 +106,12 @@ firebase.auth().onAuthStateChanged((user)=> {
             });                  
         
         
-    }   
-  }else {
+        }   
 
-  }
+    }
+    
+    }else {
+
+    }
+
 })
